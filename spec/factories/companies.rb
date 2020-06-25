b6d2cca56d72ba72aa5c87072550ddc245b9848b
Christian Moreira <chmleave@gmail.com>
2020-06-25 14:46:04 -0400

    Comitdsa

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :company do
+    fantasy_name { 'Mc Donalds' }
+    corporate_name { 'Arcos Dourados' }
+    email { Faker::Internet.email }
+    document_number { Faker::Company.brazilian_company_number(formatted: true) }
+    address { Faker::Address.city }
   end
 end
