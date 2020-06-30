32d304d90eda300975ebdbaf339bfadeb7cbc059
Christian Moreira <chmleave@gmail.com>
2020-06-30 16:51:11 -0400

    cliente pode cadastrar empresa

--- spec/features/client/client_needs_register_company_spec.rb
+++ spec/features/client/client_needs_register_company_spec.rb
@@ -20,4 +20,11 @@ feature 'Client do login' do
     expect(page).not_to have_content(I18n.t('check_company', scope: %i[company sessions]))
     expect(current_path).not_to eq new_company_path
   end
+
+  scenario 'end visit try see company' do
+    visit new_company_path
+
+    expect(current_path).to eq new_client_session_path
+    expect(page).to have_content('Para continuar, efetue login ou registre-se.')
+  end
 end
