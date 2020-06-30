966ff2d294158c25cf0e5a90696b7f977aaf0099
Christian Moreira <chmleave@gmail.com>
2020-06-30 15:12:58 -0400

    Cliente pode registrar empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,14 @@
 class CompaniesController < ApplicationController
+  before_action :authenticate_client!
+
   def new
     @company = Company.new
   end
 
   def create
+    company_created_msg = t('company_created', scope: %i[company sessions])
     @company = current_client.build_company(company_params)
-    return passed if @company.save
+    return redirect_to @company, alert: company_created_msg if @company.save
 
     render 'new'
   end
@@ -17,11 +20,7 @@ class CompaniesController < ApplicationController
   private
 
   def company_params
-    params.require(:company).permit(:fantasy_name, :corporate_name, :email, :document_number, :address)
-  end
-
-  def passed
-    flash[:alert] = 'Empresa criada com sucesso'
-    redirect_to @company
+    params.require(:company).permit(:fantasy_name, :corporate_name,
+                                    :email, :document_number, :address)
   end
 end
