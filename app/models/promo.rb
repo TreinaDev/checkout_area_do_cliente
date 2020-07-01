ffb7cf1d79baa4f9ed3c5ad5ebe713d0b6a4e3b9
Alexandre Lima <lima@fatecsp.br>
2020-07-01 15:31:02 -0300

    Finalizando a tarefa e resolvendo o problema do rubocop

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -7,14 +7,14 @@ class Promo < ApplicationRecord
   validate :end_date_must_be_valid
 
   def start_date_must_be_valid
-    if start_date.present? && start_date < Time.zone.today
-      errors.add(:start_date, 'Não é uma data válida')
-    end
+    return unless start_date.present? && start_date < Time.zone.today
+
+    errors.add(:start_date, 'Não é uma data válida')
   end
 
   def end_date_must_be_valid
-    if end_date.present? && end_date <= Time.zone.today
-      errors.add(:end_date, 'Não é uma data válida')
-    end
+    return unless end_date.present? && end_date <= Time.zone.today
+
+    errors.add(:end_date, 'Não é uma data válida')
   end
 end
