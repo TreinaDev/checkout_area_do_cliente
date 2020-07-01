d873d7dad9465ab3b9d73ea21faa0cb91179c2f1
reginadiana <dianaregina22@outlook.com.br>
2020-06-30 22:52:06 -0300

    Corrigindo arq. seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,5 @@ Employee.create(email: 'third@teste.com', password: '123456')
 	OrderClient.create(token: Faker::Code.nric, plan: Faker::Name.name)
 end
 
-Aproved.create(order_client_id: OrderClient.first.id,
-							 employee_id: first_employee.id)
-Aproved.create(order_client_id: OrderClient.last.id,
-							 employee_id: first_employee.id)
+ApprovedOrder.create(order_client_id: OrderClient.first.id)
+ApprovedOrder.create(order_client_id: OrderClient.last.id)
