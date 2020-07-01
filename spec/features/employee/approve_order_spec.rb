d9ebcc9fc365bc4fae40508b5e00f59597606fef
reginadiana <dianaregina22@outlook.com.br>
2020-06-30 22:18:10 -0300

    Vendedor aprova pedido

--- spec/features/employee/approve_order_spec.rb
+++ spec/features/employee/approve_order_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 feature 'Employee view orders' do
-  before(:each) do
+  scenario 'successfully' do
     employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
     login_as employee, scope: :employee
-    visit plans_path
-  end
-
-  scenario 'successfully' do
     order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
 
+    visit plans_path
     click_on 'Pedidos'
 
     expect(current_path).to eq(order_clients_path)
@@ -20,9 +17,11 @@ feature 'Employee view orders' do
   end
 
   scenario 'and view details' do
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    login_as employee, scope: :employee
     order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
 
-    click_on 'Pedidos'
+    visit order_clients_path
     click_on order.token
 
     expect(page).to have_content(order.token)
@@ -31,11 +30,27 @@ feature 'Employee view orders' do
     expect(page).not_to have_content('Status: Aprovado')
     expect(page).to have_link('Aprovar Pedido', href: order_client_approved_orders_path(order.id))
   end
+  scenario 'and not view details of other order' do
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    login_as employee, scope: :employee
+    order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
+    other_order = create(:order_client, token: 'AAAAAA', plan: 'Extraordinário')
+
+    visit order_clients_path
+
+    click_on 'FHDBFHDB'
+    expect(page).to have_content(order.token)
+    expect(page).to have_content(order.plan)
+    expect(page).not_to have_content(other_order.token)
+    expect(page).not_to have_content(other_order.plan)
+  end
 
   scenario 'and approve order' do
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    login_as employee, scope: :employee
     order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
 
-    click_on 'Pedidos'
+    visit order_clients_path
     click_on order.token
     click_on 'Aprovar Pedido'
 
@@ -46,22 +61,25 @@ feature 'Employee view orders' do
   end
 
   scenario 'and other order remains unchanged' do
-    order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
-    other_order = create(:order_client, token: 'AAAAAA', plan: 'Extraordinário')
-    click_on 'Pedidos'
-    expect(current_path).to eq(order_clients_path)
-    expect(page).to have_content(order.token)
-    expect(page).to have_content(order.plan)
-    expect(page).to have_content(other_order.token)
-    expect(page).to have_content(other_order.plan)
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    login_as employee, scope: :employee
 
-    find("link_to_aproved_#{order.id}").click
+    create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
+    create(:order_client, token: 'AAAAAA', plan: 'Extraordinário')
 
-    # expect(order).to have_content(other_order.plan)
-    # expect(page).to have_content(other_order.plan)
+    visit order_clients_path
+
+    click_on 'FHDBFHDB'
+    click_on 'Aprovar Pedido'
+    click_on 'Voltar'
+    click_on 'AAAAAA'
+    expect(page).to have_content('Status: Aguardando aprovação')
   end
   scenario 'but no have any order' do
-    click_on 'Pedidos'
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    login_as employee, scope: :employee
+    visit order_clients_path
+
     expect(current_path).to eq(order_clients_path)
     expect(page).to have_content('Nenhum pedido cadastrado')
   end
