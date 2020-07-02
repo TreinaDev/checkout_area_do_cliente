9bbd1a83b9d11ab97492d2cfc120a6d1a50edc23
reginadiana <dianaregina22@outlook.com.br>
2020-07-02 16:00:18 -0300

    Retirando privatede approved orders controller

--- app/controllers/approved_orders_controller.rb
+++ app/controllers/approved_orders_controller.rb
@@ -2,16 +2,9 @@ class ApprovedOrdersController < ApplicationController
   def create
     @approved_order = ApprovedOrder.create(params.permit(:order_client))
     @approved_order.save
-    # @approved_order.order_client = @order_client
     @order_client = OrderClient.find(params[:order_client_id])
     @order_client.accepted!
 
     redirect_to @order_client
-  end
-
-  private
-
-  def require_params
-    params.require(:approved_order).permit(:order_client)
   end
 end
