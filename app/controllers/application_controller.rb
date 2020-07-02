b9709bbc82ecd3807799d3e9064099b32b4b6b77
reginadiana <dianaregina22@outlook.com.br>
2020-07-02 16:36:17 -0300

    WR: pendencias do rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
   end
   
   def authenticate_visitor
-    if !current_client and !current_employee
-      redirect_to root_path
-    end
+    return if current_client || current_employee
+
+    redirect_to root_path, alert: 'Faça login para continuar'
   end
 end
