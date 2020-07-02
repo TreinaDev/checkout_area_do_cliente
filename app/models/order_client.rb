fc9538fa715134e9eeee4242ec60a331928babe7
reginadiana <dianaregina22@outlook.com.br>
2020-07-02 14:24:08 -0300

    Corrigindo algumas pendencias

--- app/models/order_client.rb
+++ app/models/order_client.rb
@@ -1,4 +1,4 @@
 class OrderClient < ApplicationRecord
-  enum status: { waiting: 0, accepted: 1, rejected: 2 }
+  enum status: { waiting: 0, accepted: 5, rejected: 10 }
   has_one :approved_order, dependent: :destroy
 end
