f0a0774e5e2b41572685b39bcea1d57fe897a83e
reginadiana <dianaregina22@outlook.com.br>
2020-07-03 12:46:01 -0300

    Layout da pagina de login do cliente

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,9 @@ class ApplicationController < ActionController::Base
     return unless current_client
     return if current_client && current_client.company.present?
 
-    redirect_to new_company_path, alert: t('check_company',
-                                           scope: %i[company sessions])
+    redirect_to new_company_path, alert: t('check_company', scope: %i[company sessions])
   end
-  
+
   def authenticate_visitor
     return if current_client || current_employee
 
