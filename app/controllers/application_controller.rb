b1dc7b75635630a100d528286ea1549f736d9946
Diana Regina <dianaregina22@outlook.com.br>
2020-07-03 16:40:34 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     return unless current_client
     return if current_client && current_client.company.present?
 
-    redirect_to new_company_path, alert: t('check_company', scope: %i[company sessions])
+    redirect_to new_company_path, alert: t('check_company', 
+                                            scope: %i[company sessions])
   end
 end
