550c57a65483659c7d5d974da3445df305f25393
TenaniLucas <lucashtenani@gmail.com>
2020-07-07 18:38:15 -0300

    Ajustando API

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -19,18 +19,20 @@ class Plan
   end
 
   def self.all
-    json = JSON.parse(response, symbolize_names: true)
-    result = json.map do |hash|
-      new(id: hash[:id], name: hash[:name], platforms: hash[:platforms],
-          limit_daily_chat: hash[:limit_daily_chat], limit_monthly_chat: hash[:limit_monthly_chat],
-          limit_daily_messages: hash[:limit_daily_messages], limit_monthly_messages: hash[:limit_monthly_messages],
-          extra_message_price: hash[:extra_message_price], extra_chat_price: hash[:extra_chat_price],
-          current_price: hash[:current_price])
+    response = Faraday.get("localhost:4000/api/v1/plans")
+    if response.success?
+      json = JSON.parse(response.body, symbolize_names: true)
+      result = json.map do |hash|
+        new(id: hash[:id], name: hash[:name], platforms: hash[:platforms],
+            limit_daily_chat: hash[:limit_daily_chat], limit_monthly_chat: hash[:limit_monthly_chat],
+            limit_daily_messages: hash[:limit_daily_messages], limit_monthly_messages: hash[:limit_monthly_messages],
+            extra_message_price: hash[:extra_message_price], extra_chat_price: hash[:extra_chat_price],
+            current_price: hash[:current_price])
+      end
+      return result
+    else # erro: 500, 403..
+      return []
     end
-    result
   end
 
-  def self.response
-    File.read(Rails.root.join('spec/fixtures/plans.json'))
-  end
 end
--- spec/features/plan/get_plans_api_spec.rb
+++ spec/features/plan/get_plans_api_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 feature 'Visitor see plans in home page' do
   describe Plan do
     it 'should get all states' do
+      raw_json = File.read(Rails.root.join('spec/fixtures/plans.json'))
+      url = 'localhost:4000/api/v1/plans'
+      fake_response = double('faraday_response', body: raw_json, status: 200)      
+      allow(Faraday).to receive(:get).with(url).and_return(fake_response)
+
       plans = Plan.all
       expect(plans[0].name).to eq 'totam'
       expect(plans[0].platforms).to eq 'Whatsapp'
@@ -15,10 +20,12 @@ feature 'Visitor see plans in home page' do
       expect(plans[0].current_price).to eq 67.27
     end
 
-    it 'should return empty array if API error' do
-      allow(Plan).to receive(:all).and_return([])
+    it 'should return empty array if API error' do      
+      url = 'localhost:4000/api/v1/plans'
+      response = double('faraday_response', body: "{}", status: 500)
+      allow(Faraday).to receive(:get).with(url).and_return(response)
 
-      result = Plan.all
+      result = StateInfo.all
       expect(result.length).to eq 0
     end
   end
