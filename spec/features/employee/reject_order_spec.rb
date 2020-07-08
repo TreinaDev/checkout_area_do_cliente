ba41b8b0dde3b30358541d0085aaf4ef5a46d72a
reginadiana <dianaregina22@outlook.com.br>
2020-07-08 20:38:49 -0300

    Resolvendo pendencias do rubocop Co-authored-by: librity <luis.geniole@gmail.com>

--- spec/features/employee/reject_order_spec.rb
+++ spec/features/employee/reject_order_spec.rb
@@ -46,8 +46,6 @@ feature 'Employee reject orders' do
     expect(current_path).to eq(new_order_client_rejected_order_path(order))
     fill_in 'Motivo', with: ' '
     click_on 'Enviar'
-    
-    # expect(current_path).to eq(order_client_rejected_order_path(order))
     expect(page).to have_content('Motivo não pode ficar em branco')
   end
 end
