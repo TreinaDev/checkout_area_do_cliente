bbdc01e9e7b1fe5ec279d53cf7a89a31296b571a
Christian Moreira <chmleave@gmail.com>
2020-07-09 17:08:21 -0400

    Criando teste

--- app/controllers/approved_orders_controller.rb
+++ app/controllers/approved_orders_controller.rb
@@ -1,9 +1,24 @@
 class ApprovedOrdersController < ApplicationController
+
   def create
     @order_client = OrderClient.find(params[:order_client_id])
-    @approved_order = @order_client.create_approved_order
-    @order_client.accepted!
-
+    sync_api
     redirect_to @order_client
+  end
+
+  private
+
+  def sync_api
+    url = 'http://localhost:3000/api/v1/purchases'
+    response = Faraday.post(url, { company_token: @order_client.token,
+                                   plan_id: @order_client.plan_id })
+    if response.status == 200
+      @approved_order = @order_client.create_approved_order
+      @order_client.accepted!
+      byebug
+      # @approved_order.bot_token = response.body['bot']['token']
+    else
+      redirect_to @order_client, notice: t('.notice')
+    end
   end
 end
--- spec/features/employee/order_to_api_spec.rb
+++ spec/features/employee/order_to_api_spec.rb
@@ -2,12 +2,51 @@ require 'rails_helper'
 
 feature 'Order to api' do
   scenario 'successfully' do
+    client = client_login
+    company = create(:company, client: client)
     employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples', plan_id: 1, client: client)
+    
+    url = 'http://localhost:3000/api/v1/purchases'
+    json = { company_token: order.token, plan_id: order.plan_id }
+    response_json = { company: { name: company.fantasy_name }, plan: { name: 'Simples' }, bot: { token: 'ABC123'} }
+    response = double('faraday_response', body: response_json, status: 200)
+
+    allow(Faraday).to receive(:post).with(url, json).and_return(response)
+    
+    
     login_as employee, scope: :employee
-    order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples')
 
     visit order_clients_path
     click_on order.token
     click_on 'Aprovar Pedido'
+
+    expect(order.status).to eq :accepted
+    # expect(order).to be_accepted
+    expect(order.bot_token).to eq 'ABAC123'
+  end
+
+  scenario 'failed' do
+    client = client_login
+    company = create(:company, client: client)
+    employee = create(:employee, email: 'vendedor@empresa.com', password: '123456')
+    order = create(:order_client, token: 'FHDBFHDB', plan: 'Simples', plan_id: 1, client: client)
+    
+    url = 'http://localhost:3000/api/v1/purchases'
+    json = { company_token: order.token, plan_id: order.plan_id }
+    response_json = { company: { name: company.fantasy_name }, plan: { name: 'Simples' }, bot: { token: 'ABC123'} }
+    response = double('faraday_response', body: response_json, status: 500)
+
+    allow(Faraday).to receive(:post).with(url, json).and_return(response)
+    
+    
+    login_as employee, scope: :employee
+
+    visit order_clients_path
+    click_on order.token
+    click_on 'Aprovar Pedido'
+
+    expect(order.accepted?).to be_true
+    
   end
 end
