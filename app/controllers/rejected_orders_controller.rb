1c993739237f7ae0bc01271a99c1686d008ce34f
Luis Geniole <luis.geniole@gmail.com>
2020-07-09 11:37:19 -0300

    Review do Henrique.
    Co-authored-by: reginadiana <dianaregina22@outlook.com.br>

--- app/controllers/rejected_orders_controller.rb
+++ app/controllers/rejected_orders_controller.rb
@@ -7,11 +7,10 @@ class RejectedOrdersController < ApplicationController
   def create
     @order_client = OrderClient.find(params[:order_client_id])
     @rejected_order = @order_client.create_rejected_order(rejected_order_params)
-    @order_client.rejected!
 
     if @rejected_order.save
-      flash[:notice] = 'Pedido rejeitado com sucesso'
-      redirect_to @order_client
+      @order_client.rejected!
+      redirect_to @order_client, notice: t('flash.order.rejected')
     else
       render :new
     end
--- spec/models/rejected_order_spec.rb
+++ spec/models/rejected_order_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe RejectedOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create :rejected_order }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
 end
