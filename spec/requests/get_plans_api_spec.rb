cf6a14a04ad72e8d17e46724b8b11e07c8333010
Henrique Morato <riquemorato@gmail.com>
2020-07-13 12:49:54 -0300

    Usando a api

--- spec/requests/get_plans_api_spec.rb
+++ spec/requests/get_plans_api_spec.rb
@@ -8,16 +8,16 @@ feature 'Visitor see plans in home page' do
       expect(plans[0].name).to eq 'totam'
       expect(plans[0].platforms).to eq 'Whatsapp'
       expect(plans[0].limit_daily_chat).to eq 1
-      expect(plans[0].limit_monthly_chat).to eq 1
-      expect(plans[0].limit_daily_messages).to eq 1
-      expect(plans[0].limit_monthly_messages).to eq 1
-      expect(plans[0].extra_message_price).to eq 1.5
-      expect(plans[0].extra_chat_price).to eq 1.5
+      expect(plans[0].limit_monthly_chat).to eq 55
+      expect(plans[0].limit_daily_messages).to eq 11
+      expect(plans[0].limit_monthly_messages).to eq 21
+      expect(plans[0].extra_message_price).to eq 1.6
+      expect(plans[0].extra_chat_price).to eq 1.8
       expect(plans[0].current_price).to eq 67.27
     end
 
     it 'should return empty array if API error' do
-      stub_request(:get, 'http://exemplo.com/api/v1/plans/')
+      stub_request(:get, 'http://exemplo.com/api/v1/plans')
         .to_return(status: 500)
       plans = Plan.all
 
